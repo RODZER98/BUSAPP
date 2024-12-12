68bc59c31c4306c8060309a3879041f18f336748
Rodney G <rodneygallardo98@gmail.com>
2024-12-12 14:58:10 -0400

    "segunda_subida"

---
+++ app.js
@@ -0,0 +1,21 @@
+//require('dotenv').config()
+const express = require('express')
+const app = express()
+//const mongoose = require('mongoose')
+const path = require('path')
+
+//conexion a BD
+
+    /*try{
+        mongoose.connect(process.env.MONGO_URL)
+        console.log('Conexion a BD correcta')
+    } catch(error) {
+        console.log(error)
+    }*/
+
+
+
+//crear rutas de front end
+
+app.use('/', express.static(path.resolve('views','home')))
+module.exports = app
